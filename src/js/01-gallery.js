3eade146205af8989b37e11a6fab14768c8af0f3
mihaiciubotaru <mihai.cpp@gmail.com>
2024-08-28 12:56:01 +0300

    after final corrections

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,20 +4,6 @@ const ul = document.getElementsByClassName("gallery");
 let s = "";
 
 for(let i = 0; i<galleryItems.length; i++){
-    console.log(galleryItems[i].preview);
     s += `<div class="grid-item"><img src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" id="img${i}"></div>\n`;
 }
 ul[0].innerHTML = s;
-
-document.addEventListener("click", (event) => showModalImg(event.target.id));
-
-function showModalImg(id){
-    if (id.length === 0 ) return;
-    
-    const image = basicLightbox.create(
-        `<img width="1400" height="900" src="${galleryItems[id.substring(3)].original}">`,
-        { closable: true }
-      );
-    image.show();
-
-}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const li = document.getElementsByClassName("gallery__item");
+let s = "";
+
+for(let i = 0; i<galleryItems.length; i++){
+    s += `<a class="gallery__link" href="${galleryItems[i].original}">\n`;
+    s += `  <img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" title="${galleryItems[i].description}" />\n`;
+    s += `</a>\n`;
+}
+li[0].innerHTML = s;
+
+new SimpleLightbox('.gallery__item a', {
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    captionData: 'alt'
+  });

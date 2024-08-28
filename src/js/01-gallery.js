ff61e0c7e5cf25a3784eea35baaca836d7668c22
mihaiciubotaru <mihai.cpp@gmail.com>
2024-08-28 16:48:11 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,3 +7,15 @@ for(let i = 0; i<galleryItems.length; i++){
     s += `<div class="grid-item"><img src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" id="img${i}"></div>\n`;
 }
 ul[0].innerHTML = s;
+
+document.addEventListener("click", (event) => showModalImg(event.target.id));
+
+function showModalImg(id){
+    if (id.length === 0 ) return;
+    
+    const image = basicLightbox.create(
+        `<img width="1400" height="900" src="${galleryItems[id.substring(3)].original}">`,
+        { closable: true }
+      );
+    image.show();
+}
